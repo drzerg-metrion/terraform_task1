d6c218d424e567384f08b9a5947d6af3094fd04f
serhii_kosharuk <drzerg.metrion@gmail.com>
2021-03-04 12:35:49 +0200

    start. not working

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  default = "services-exp-labs-1"
+}
+
+variable "region" {
+  default = "us-west1" 
+}
+
+variable "zone"  {
+  default = "us-west1-a"
+}
+
+variable "my_machine_type" {
+  default = "f1-micro"
+}
+
+variable "my_image_type" {
+  default = "centos-cloud/centos-7"
+}
+
